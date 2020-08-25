5a3b27689668b333cc84743d26651fcb581e8a8e
ashut08 <ashusingh.unch8@gmail.com>
2020-08-25 09:04:55 +0530

    add templates

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-﻿from flask import Flask
+﻿from flask import Flask,render_template
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/home")
 def hello_world():
-    return 'Hello World'
-   
+    return render_template('home.html')
 
-if __name__ == '__main__':
-   app.run()
+@app.route("/about")
+def about():
+    return "<h1> About page </h1>"
+
